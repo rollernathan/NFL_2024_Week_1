3ed3920af2579960c5bb8fb1446f75fd4c69b98b
rollernathan <roller.nathan@gmail.com>
2024-09-16 20:24:49 -0700

    website updated

--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,45 @@
 // Build the metadata panel
-function buildMetadata(sample) {
+function buildMetadata(playId) {
   d3.csv("https://rollernathan.github.io/NFL_2024_Week_1/resources/play_by_play_2024_cleaned.csv").then((data) => {
 
-    // get the metadata field
-    const metadata = data.metadata;
+  // Find the data corresponding to the selected play_id
+  const result = data.find(item => item.play_id === playId);
 
-    // Filter the metadata for the object with the desired sample number
-    const result = metadata.find(entry => entry.id === parseInt(sample));
-
-    // Use d3 to select the panel with id of `#sample-metadata`
+    // Use d3 to select the panel with id of `#play_data`
     const panel = d3.select("#play_data");
 
     // Use `.html("") to clear any existing metadata
     panel.html("");
 
-    // Inside a loop, you will need to use d3 to append new
-    // tags for each key-value in the filtered metadata.
-    Object.entries(result).forEach(([key, value]) => {
-      panel.append("p").text(`${key.toUpperCase()}: ${value}`);
-    });
+    // Check if result is found
+    if (result) {
+      // Append each key-value pair as a paragraph
+      Object.entries(result).forEach(([key, value]) => {
+        panel.append("p").text(`${key.toUpperCase()}: ${value}`);
+      });
+    } else {
+      panel.append("p").text("No data found for this play_id.");
+    };
   });
-}
+};
 
 // Function to run on page load
 function init() {
   d3.csv("https://rollernathan.github.io/NFL_2024_Week_1/resources/play_by_play_2024_cleaned.csv").then((data) => {
     console.log(data);
     
-    // Get the names field
-    const names = data.names;
-
     // Use d3 to select the dropdown with id of `#selDataset`
     const dropdown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-    names.forEach(name => {
-      dropdown.append("option").text(name).property("value", name);
+    data.forEach(item => {
+      dropdown.append("option").text(item.play_id).property("value", item.play_id);
     });
 
     // Get the first sample from the list
-    const firstSample = names[0];
+    const firstSample = data[0].play_id;
 
     // Build metadata panel with the first sample
     buildMetadata(firstSample);
